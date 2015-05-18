a588cf3ae381b239618fd05fa8b095e65ef06956
Галя Георгиева <alice.gospodinova@gmail.com>
2015-05-18 09:22:04 +0300

    change names of var: int player2X -> int player2Position;  int player1X -> int player1Position; implemented methods:  MoveFirstPlayer(); MovePl -> needed methods: DrawPlayer1(); DrawPlayer2(); DrawBall();

--- PongGame.cs
+++ PongGame.cs
@@ -7,17 +7,18 @@ class PongGame
     static int BallY;
     static bool allMovingRight = true;
     static bool allMovingUp = true;
-    static int player1InitialX=0;
-    static int player1InitialY= (Console.WindowHeight-player1BatSize)/2;
-    static int player1X;
-    static int player2IntialX=Console.WindowWidth-1;
+    static int player1InitialX = 0;
+    static int player1InitialY = (Console.WindowHeight - player1BatSize) / 2;
+    static int player1Position;
+    static int player2IntialX = Console.WindowWidth - 1;
     static int player2InitialY = (Console.WindowHeight - player2BatSize) / 2;
-    static int player2X;
-    static int player1BatSize=5;
-    static int player2BatSize=5;
+    static int player2Position;
+    static int player1BatSize = 5;
+    static int player2BatSize = 5;
     static int player1Score = 0;
     static int player2Score = 0;
     static Random randomNum = new Random();
+
 
     static void Main()
     {
@@ -33,15 +34,32 @@ class PongGame
     }
     static void RemoveScrollBars()
     {
-        Console.WindowHeight = 40;
-        Console.WindowWidth = 80;
+        //Console.WindowHeight = 40;
+        //Console.WindowWidth = 80;
         Console.ForegroundColor = ConsoleColor.White;
         Console.BufferHeight = Console.WindowHeight;
         Console.BufferWidth = Console.WindowWidth;
     }
     private static void SetInitialPositions()
     {
-        //TODO: Set ball and players position before start of the game
+        //TODO: Set ball and players position before start of the game and draw them
+        DrawPlayer1();
+        DrawPlayer2();
+        DrawBall();
+    }
+
+    private static void DrawBall()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void DrawPlayer2()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void DrawPlayer1()
+    {
         throw new NotImplementedException();
     }
 
@@ -57,15 +75,41 @@ class PongGame
         throw new NotImplementedException();
     }
 
-    private static void MovePlayer2()
+    private static void MoveSecondPlayer()
     {
-        //TODO: implement how to move first palayer (AI) - in the left side of the field
+        //TODO: implement how to move first palayer
         throw new NotImplementedException();
     }
 
-    private static void MovePlayer1()
+    private static void MoveFirstPlayer()
     {
-        //TODO: implement how to move first palayer - in the right side of the field
-        throw new NotImplementedException();
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo whatIsTheKey = Console.ReadKey();
+            if (whatIsTheKey.Key == ConsoleKey.UpArrow)
+            {
+                MovePlayer1UP();
+            }
+            else
+            {
+                MovePlayer1DOWN();
+            }
+        }
+    }
+
+    private static void MovePlayer1UP()
+    {
+        if (player1Position > 0)
+        {
+            player1Position--;
+        }
+    }
+
+    private static void MovePlayer1DOWN()
+    {
+        if (player1Position < Console.WindowHeight - player1BatSize)
+        {
+            player1Position++;
+        }
     }
 }
