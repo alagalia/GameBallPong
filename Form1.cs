c65d07d689c267917acb4c6f51f1ade690784f44
Alerionx3 <marsata1@yahoo.com>
2015-05-27 13:16:18 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -15,6 +15,35 @@ namespace WindowsFormsApplication1
         private int initialPlayer1Y = 230;
         private int initialPlayer2X = 12;
         private int initialPlayer2Y = 230;
+        private bool secondPlayer = true;
+        private int difficulty = 90;
+        private bool movingRight = true;
+        private bool movingUp = true;
+        private int ballSpeed = 20;
+        private void Move_AI(int diffictulty)
+        {
+            Random rnd = new Random();
+            int tmp = rnd.Next(0, 101);
+            if (tmp < diffictulty)
+            {
+                if (movingUp)
+                {
+                    if (!pictureBox2.Bounds.IntersectsWith(pictureBox3.Bounds))
+                    {
+                        Player2Y -= ballSpeed;
+                        pictureBox2.Location = new Point(Player2X, Player2Y);
+                    }
+                }
+                else
+                {
+                    if (!pictureBox2.Bounds.IntersectsWith(pictureBox4.Bounds))
+                    {
+                        Player2Y += ballSpeed;
+                        pictureBox2.Location = new Point(Player2X, Player2Y);
+                    }
+                }
+            }
+        }
 
         public Form1()
         {
@@ -24,6 +53,8 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
         }
+
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
 
@@ -36,6 +67,25 @@ namespace WindowsFormsApplication1
                 if (!pictureBox1.Bounds.IntersectsWith(pictureBox4.Bounds)) Player1Y += PlayerSpeed;
             }
             pictureBox1.Location = new Point(Player1X, Player1Y);
+            if (secondPlayer)
+            {
+                if (e.KeyCode == Keys.W)
+                {
+                    if (!pictureBox2.Bounds.IntersectsWith(pictureBox3.Bounds)) Player2Y -= PlayerSpeed;
+                }
+                if (e.KeyCode == Keys.S)
+                {
+                    if (!pictureBox2.Bounds.IntersectsWith(pictureBox4.Bounds)) Player2Y += PlayerSpeed;
+                }
+                pictureBox2.Location = new Point(Player2X, Player2Y);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!secondPlayer) Move_AI(difficulty);
+            Invalidate();
+
         }
     }
 }
