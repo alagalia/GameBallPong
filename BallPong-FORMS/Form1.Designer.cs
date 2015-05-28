27733ecb7a99d2ccd1a0951571c1b0d609463784
Alerionx3 <marsata1@yahoo.com>
2015-05-28 09:59:27 +0300

    All FIles Update

---
+++ BallPong-FORMS/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.secondPlayerNameLabel = new System.Windows.Forms.Label();
+            this.firstPlayerNameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.player2PictureBox = new System.Windows.Forms.PictureBox();
+            this.player1PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // secondPlayerNameLabel
+            // 
+            this.secondPlayerNameLabel.AutoSize = true;
+            this.secondPlayerNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.secondPlayerNameLabel.Font = new System.Drawing.Font("Myriad Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondPlayerNameLabel.ForeColor = System.Drawing.Color.White;
+            this.secondPlayerNameLabel.Location = new System.Drawing.Point(537, 5);
+            this.secondPlayerNameLabel.Name = "secondPlayerNameLabel";
+            this.secondPlayerNameLabel.Size = new System.Drawing.Size(45, 16);
+            this.secondPlayerNameLabel.TabIndex = 2;
+            this.secondPlayerNameLabel.Text = "secondPlayerNameLabel";
+            // 
+            // firstPlayerNameLabel
+            // 
+            this.firstPlayerNameLabel.AutoSize = true;
+            this.firstPlayerNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.firstPlayerNameLabel.Font = new System.Drawing.Font("Myriad Pro", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstPlayerNameLabel.ForeColor = System.Drawing.Color.White;
+            this.firstPlayerNameLabel.Location = new System.Drawing.Point(664, 5);
+            this.firstPlayerNameLabel.Name = "firstPlayerNameLabel";
+            this.firstPlayerNameLabel.Size = new System.Drawing.Size(45, 16);
+            this.firstPlayerNameLabel.TabIndex = 3;
+            this.firstPlayerNameLabel.Text = "firstPlayerNameLabel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Fipps", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(568, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 57);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Fipps", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(646, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 57);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Fipps", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(609, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 57);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Fipps", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(467, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(318, 144);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "SCORE!";
+            this.label6.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Fipps", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(595, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 144);
+            this.label7.TabIndex = 10;
+            this.label7.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApplication1.Properties.Resources.arrow2;
+            this.pictureBox5.Location = new System.Drawing.Point(97, 568);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(88, 63);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(-2, 725);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1247, 10);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(0, -9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1247, 10);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // player2PictureBox
+            // 
+            this.player2PictureBox.Location = new System.Drawing.Point(12, 230);
+            this.player2PictureBox.Name = "player2PictureBox";
+            this.player2PictureBox.Size = new System.Drawing.Size(22, 179);
+            this.player2PictureBox.TabIndex = 1;
+            this.player2PictureBox.TabStop = false;
+            // 
+            // player1PictureBox
+            // 
+            this.player1PictureBox.Location = new System.Drawing.Point(1212, 230);
+            this.player1PictureBox.Name = "player1PictureBox";
+            this.player1PictureBox.Size = new System.Drawing.Size(22, 179);
+            this.player1PictureBox.TabIndex = 0;
+            this.player1PictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1246, 724);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.firstPlayerNameLabel);
+            this.Controls.Add(this.secondPlayerNameLabel);
+            this.Controls.Add(this.player2PictureBox);
+            this.Controls.Add(this.player1PictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pong";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1PictureBox;
+        private System.Windows.Forms.PictureBox player2PictureBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label secondPlayerNameLabel;
+        private System.Windows.Forms.Label firstPlayerNameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+
+    }
+}
+
